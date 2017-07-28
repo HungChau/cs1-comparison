47af622312ce4f83de4dc4175e2e507356734385
HUNG CHAU <hungchaukim@gmail.com>
2017-07-28 17:02:10 -0400

    updated

--- parse_concept.py
+++ parse_concept.py
@@ -1,1 +1,8 @@
+import requests
+
+
 print ("Input a code program, and then parse the concepts")
+
+data = {'code': "a =10\nprint(a)",'mode': "concepts"}
+req = requests.post(url = 'http://acos.cs.hut.fi/python-parser', data=data)
+print(req.text)
